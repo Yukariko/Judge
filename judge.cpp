66a7bf471aeae916dc7514f97eaed5e6b955e627
Yukariko <rkdals7979@naver.com>
2015-11-10 20:02:50 +0900

    bug fix

--- judge.cpp
+++ judge.cpp
@@ -1,8 +1,8 @@
 #include "judge.h"
 #include "compile.h"
 
-Judge::Judge(Language lang, const string& codePath, const string& inPath, const string& outPath)
-	: lang(lang), codePath(codePath), inPath(inPath), outPath(outPath) 
+Judge::Judge(Language& lang, const string& codePath, const string& inPath, const string& outPath)
+//	: lang(lang), codePath(codePath), inPath(inPath), outPath(outPath) 
 {
 	Compile compileManager(lang, codePath);
 
@@ -13,6 +13,8 @@ Judge::Judge(Language lang, const string& codePath, const string& inPath, const 
 	{
 
 	}
+
+	// compile fail
 	else
 	{
 
